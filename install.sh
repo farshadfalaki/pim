ed4dc1de611e0a20ed8a24175a8121be50bebfba
Falaki <farshad.falaki@gmail.com>
2019-08-06 03:49:14 +0430

    adding git pull

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo '******Pulling from git'
+git init
+git pull https://github.com/farshadfalaki/pim.git
 echo '******Building the entire project ...'
 mvn clean package -DskipTests
 
