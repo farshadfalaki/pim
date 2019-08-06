7f22608b87ca4511d15bbaa1a17787e2296e4c96
Falaki <farshad.falaki@gmail.com>
2019-08-07 00:06:32 +0430

    remove unused code

--- modules/aggregator/src/main/java/com/farshad/aggregator/dto/StatisticsCommandDto.java
+++ modules/aggregator/src/main/java/com/farshad/aggregator/dto/StatisticsCommandDto.java
@@ -2,8 +2,6 @@ package com.farshad.aggregator.dto;
 
 
 import com.farshad.aggregator.constants.ProductProcessStatus;
-import com.farshad.aggregator.model.Statistics;
-
 import java.util.Date;
 
 public class StatisticsCommandDto {
@@ -17,19 +15,9 @@ public class StatisticsCommandDto {
         this.productProcessStatus = productProcessStatus;
     }
 
-    public StatisticsCommandDto(Statistics statistics){
-        /*if(statistics!=null){
-            this.setDate(statistics.getDate());
-            this.setNewItemsCount(statistics.getNewItemsCount());
-            this.setUpdatedItemsCount(statistics.getUpdatedItemsCount());
-        }*/
-    }
-
     public Date getDate() {
         return date;
     }
-
-
 
     public ProductProcessStatus getProductProcessStatus() {
         return productProcessStatus;
