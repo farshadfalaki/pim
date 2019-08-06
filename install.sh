f3e3e3670eb92085600a5e4e8999343a84402a68
Falaki <farshad.falaki@gmail.com>
2019-08-07 00:10:43 +0430

    comment git pull section

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-echo '******Pulling from git'
-git init
-git pull https://github.com/farshadfalaki/pim.git
-echo '******Building the entire project ...'
+#echo '******Pulling from git******'
+#git init
+#git pull https://github.com/farshadfalaki/pim.git
+echo '******Building the entire project******'
 mvn clean package -DskipTests
 
-echo '******Building docker files .....'
+echo '******Building docker files******'
 cd  ./modules/importer
 docker build -t importer .
 cd ../aggregator
 docker build -t aggregator .
 
-echo '******Going to run network via docker-compose .....'
+echo '******Going to run network via docker-compose******'
 docker-compose up
